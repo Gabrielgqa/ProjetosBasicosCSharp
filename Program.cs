fd0aeb840dbcec0c632894fca39d8d07da83263e
GabrielGQA <gabrielgqa@gmail.com>
2023-05-26 16:26:13 -0300

    feat: interação inicial

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Primeiro valor: ");
+float primeiroValor = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Segundo valor: ");
+float segundoValor = Convert.ToSingle(Console.ReadLine());
+
+float result = primeiroValor + segundoValor;
+Console.WriteLine("O resultado da soma é: "+ result);
+Console.WriteLine($"O resultado da soma é: {primeiroValor + segundoValor}");
+Console.WriteLine($"O resultado da soma é: {result}");
