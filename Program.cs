c2963620a4bdeb55e627b8a357b26c1a1b62c3c1
GabrielGQA <gabrielgqa@gmail.com>
2023-05-28 12:13:56 -0300

    feat: divisão e multiplicação

--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
-﻿Console.Clear();
-Console.WriteLine("Primeiro valor: ");
-float primeiroValor = Convert.ToSingle(Console.ReadLine());
-Console.WriteLine("Segundo valor: ");
-float segundoValor = Convert.ToSingle(Console.ReadLine());
+﻿Soma();
+Subtracao();
+Divisao();
+Multiplicacao();
 
-float result = primeiroValor + segundoValor;
-Console.WriteLine("O resultado da soma é: "+ result);
-Console.WriteLine($"O resultado da soma é: {primeiroValor + segundoValor}");
-Console.WriteLine($"O resultado da soma é: {result}");
+static void Soma() {
+    Console.Clear();
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Segundo valor: ");
+    float segundoValor = Convert.ToSingle(Console.ReadLine());
+
+    float resultado = primeiroValor + segundoValor;
+    Console.WriteLine($"O resultado da soma é: {resultado}");
+    Console.ReadKey();
+}
+
+static void Subtracao() {
+    Console.Clear();
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Segundo valor: ");
+    float segundoValor = Convert.ToSingle(Console.ReadLine());
+
+    float resultado = primeiroValor - segundoValor;
+    Console.WriteLine($"O resultado da subtração é: {resultado}");
+    Console.ReadKey();
+}
+
+static void Divisao() {
+    Console.Clear();
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Segundo valor: ");
+    float segundoValor = Convert.ToSingle(Console.ReadLine());
+
+    float resultado = primeiroValor / segundoValor;
+    Console.WriteLine($"O resultado da divisão é: {resultado}");
+    Console.ReadKey();
+}
+
+static void Multiplicacao() {
+    Console.Clear();
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Segundo valor: ");
+    float segundoValor = Convert.ToSingle(Console.ReadLine());
+
+    float resultado = primeiroValor * segundoValor;
+    Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+    Console.ReadKey();
+}
